bbd623f7a8cfb6f506bf3be180ad70465d2fddf9
Danny Guinther <dannyguinther@gmail.com>
2024-09-29 21:58:12 -0400

    Rename WorkerService to DefaultWorkerService

--- Reqless.Worker/DefaultWorkerService.cs
+++ Reqless.Worker/DefaultWorkerService.cs
@@ -1,4 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace Reqless.Worker;
@@ -6,13 +5,8 @@ namespace Reqless.Worker;
 /// <summary>
 /// Background service for running a worker.
 /// </summary>
-public class WorkerService : BackgroundService
+public class DefaultWorkerService : BackgroundService
 {
-    /// <summary>
-    /// The name the worker should use when communicating with Reqless.
-    /// </summary>
-    protected string _name;
-
     /// <summary>
     /// A <see cref="IServiceProvider"/> instance that is used to create a new
     /// scope when creating worker instances.
@@ -25,29 +19,25 @@ public class WorkerService : BackgroundService
     protected readonly IWorkerFactory _workerFactory;
 
     /// <summary>
-    /// Create an instance of <see cref="WorkerService"/>.
+    /// Create an instance of <see cref="DefaultWorkerService"/>.
     /// </summary>
-    /// <param name="name">The name the worker should use when communicating
-    /// with Reqless.</param>
     /// <param name="serviceProvider">A <see cref="IServiceProvider"/> instance
     /// that is used to create a new scope when creating worker instances.</param>
     /// <param name="workerFactory">A <see cref="IWorkerFactory"/> instance that
     /// is used to create worker instances.</param>
-    public WorkerService(
-        string name,
+    public DefaultWorkerService(
         IServiceProvider serviceProvider,
         IWorkerFactory workerFactory
     )
     {
-        _name = name;
         _serviceProvider = serviceProvider;
         _workerFactory = workerFactory;
     }
 
     /// <inheritdoc/>
-    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    protected override Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        IWorker worker = _workerFactory.Create(_serviceProvider, _name);
-        await worker.ExecuteAsync(cancellationToken);
+        IWorker worker = _workerFactory.Create(_serviceProvider);
+        return worker.ExecuteAsync(cancellationToken);
     }
 }
