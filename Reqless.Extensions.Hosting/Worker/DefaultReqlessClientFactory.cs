e934701ad74267cb0661647486ab054d1118fc2c
Danny Guinther <dannyguinther@gmail.com>
2024-10-06 10:13:06 -0400

    Add DefaultReqlessClientFactory implementation of IReqlessClientFactory

---
+++ Reqless.Extensions.Hosting/Worker/DefaultReqlessClientFactory.cs
@@ -0,0 +1,31 @@
+using Reqless.Client;
+using Reqless.Framework;
+
+namespace Reqless.Extensions.Hosting.Worker;
+
+/// <summary>
+/// Default implementation of <see cref="IReqlessClientFactory"/> that uses an
+/// instance of <see cref="WorkerSettings"/> for client configuration.
+/// </summary>
+public class DefaultReqlessClientFactory : IReqlessClientFactory
+{
+    private readonly IWorkerSettings _settings;
+
+    /// <summary>
+    /// Create an instance of <see cref="DefaultReqlessClientFactory"/>.
+    /// </summary>
+    /// <param name="settings">The <see cref="WorkerSettings"/> instance
+    /// that should be used to configure the clients.</param>
+    public DefaultReqlessClientFactory(IWorkerSettings settings)
+    {
+        ArgumentNullException.ThrowIfNull(settings, nameof(settings));
+
+        _settings = settings;
+    }
+
+    /// <inheritdoc />
+    public IReqlessClient Create()
+    {
+        return new ReqlessClient(_settings.ConnectionString);
+    }
+}
